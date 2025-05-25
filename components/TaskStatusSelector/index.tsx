e425465f68eec3aeaaf6d68c72eda79247888c1b
ngoctong-3107 <ngoctong3107@gmail.com>
2025-05-25 23:02:11 +0700

    fix: project details ui

--- components/TaskStatusSelector/index.tsx
+++ components/TaskStatusSelector/index.tsx
@@ -1,66 +1,66 @@
-import { Select, Flex, Typography } from "antd";
+import { Flex, Select, Typography } from "antd";
 import TaskHook from "modules/tasks/hook/task.hook";
-import StatusDot from "./StatusDot";
 import {
-	type TaskMetadataForDropdown,
-	TaskStatus,
+  type TaskMetadataForDropdown,
+  TaskStatus,
 } from "modules/tasks/types/task.types";
 import { type CSSProperties, useState } from "react";
 import { Neutral } from "styles/colors";
+import StatusDot from "./StatusDot";
 
 const { Text } = Typography;
 
 type TaskStatusSelectorProps = {
-	value?: TaskStatus;
-	onChange: (value: TaskStatus) => void;
+  value?: TaskStatus;
+  onChange: (value: TaskStatus) => void;
 };
 
 const STYLE: CSSProperties = {
-	width: "100%",
-	minWidth: 120,
-	border: `1.5px solid ${Neutral[300]}`,
-	borderRadius: 8,
-	boxShadow: `0px 0.5px 4px ${Neutral[200]}`,
+  width: "fit-content",
+  // minWidth: 120,
+  border: `1.5px solid ${Neutral[300]}`,
+  borderRadius: 8,
+  boxShadow: `0px 0.5px 4px ${Neutral[200]}`,
 };
 
 const TaskStatusSelector = (props: TaskStatusSelectorProps) => {
-	const { value = TaskStatus.NotStartYet } = props;
+  const { value = TaskStatus.NotStartYet } = props;
 
-	const taskMetadata = TaskHook.useGetTaskMetadata({
-		usedForDropdown: true,
-	}) as TaskMetadataForDropdown;
+  const taskMetadata = TaskHook.useGetTaskMetadata({
+    usedForDropdown: true,
+  }) as TaskMetadataForDropdown;
 
-	const [val, setVal] = useState<TaskStatus>(value);
+  const [val, setVal] = useState<TaskStatus>(value);
 
-	const options = taskMetadata.status.map((item) => ({
-		value: item.value,
-		label: (
-			<Flex align="center" gap={8}>
-				<StatusDot status={item.value as TaskStatus} />
-				<Text style={{ fontWeight: 500 }}>{item.label}</Text>
-			</Flex>
-		),
-	}));
+  const options = taskMetadata.status.map((item) => ({
+    value: item.value,
+    label: (
+      <Flex align="center" gap={8}>
+        <StatusDot status={item.value as TaskStatus} />
+        <Text style={{ fontWeight: 500 }}>{item.label}</Text>
+      </Flex>
+    ),
+  }));
 
-	const onHandleChange = (value: TaskStatus) => {
-		setVal(value);
-		props.onChange(value);
-	};
+  const onHandleChange = (value: TaskStatus) => {
+    setVal(value);
+    props.onChange(value);
+  };
 
-	return (
-		<Select
-			suffixIcon={null}
-			style={STYLE}
-			styles={{
-				popup: { root: { width: 180 } },
-			}}
-			defaultValue={val}
-			placeholder="Status"
-			variant="borderless"
-			options={options}
-			onChange={onHandleChange}
-		/>
-	);
+  return (
+    <Select
+      suffixIcon={null}
+      style={STYLE}
+      styles={{
+        popup: { root: { width: 180 } },
+      }}
+      defaultValue={val}
+      placeholder="Status"
+      variant="borderless"
+      options={options}
+      onChange={onHandleChange}
+    />
+  );
 };
 
 export default TaskStatusSelector;
--- modules/tasks/components/CreateEditTaskModal/styled.tsx
+++ modules/tasks/components/CreateEditTaskModal/styled.tsx
@@ -2,40 +2,49 @@ import { createStyles } from "antd-style";
 import { Neutral } from "styles/colors";
 
 const useStyles = createStyles(({ token }) => ({
-	wrapper: {
-		width: "100%",
-	},
+  wrapper: {
+    width: "100%",
+  },
 
-	titleInput: {
-		fontWeight: 600,
-		marginBottom: token.marginXS,
-		fontSize: 24,
-		borderBottom: `2px solid ${Neutral[200]}`,
-	},
+  titleInput: {
+    fontWeight: 600,
+    marginBottom: token.marginXS,
+    fontSize: 24,
+    borderBottom: `2px solid ${Neutral[200]}`,
+  },
 
-	mainData: {
-		padding: token.padding,
-		paddingTop: token.paddingXXS,
-	},
+  mainData: {
+    padding: token.padding,
+    paddingTop: token.paddingXXS,
+  },
 
-	metaData: {
-		marginTop: token.margin,
-		border: `1px solid ${Neutral[200]}`,
-		padding: `${token.paddingXXS}px ${token.paddingXS}px`,
-		marginRight: token.margin,
-		borderRadius: token.borderRadiusLG,
-	},
+  metaData: {
+    marginTop: token.margin,
+    border: `1px solid ${Neutral[200]}`,
+    padding: `${token.paddingXXS}px ${token.paddingXS}px`,
+    marginRight: token.margin,
+    borderRadius: token.borderRadiusLG,
+  },
 
-	metaBlock: {
-		padding: token.paddingXS,
-		":not(:last-child)": {
-			borderBottom: `1px dashed ${Neutral[200]}`,
-		},
-	},
+  metaBlock: {
+    padding: token.paddingXS,
+    ":not(:last-child)": {
+      borderBottom: `1px dashed ${Neutral[200]}`,
+    },
+  },
 
-	options: {
-		marginTop: token.margin,
-	},
+  options: {
+    marginTop: token.margin,
+  },
+
+  project: {
+    border: `1px solid ${Neutral[200]}`,
+    padding: `${token.paddingXXS}px ${token.paddingXS}px`,
+    borderRadius: token.borderRadiusLG,
+    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
+    flexBasis: 'fit-content',
+    marginLeft: '10px'
+  }
 }));
 
 export default useStyles;
